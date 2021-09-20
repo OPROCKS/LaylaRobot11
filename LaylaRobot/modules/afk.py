dc28bc225df5ccd5d23638a0937011fe90849903
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-09-20 18:43:46 +0800

    Update afk.py

--- LaylaRobot/modules/afk.py
+++ LaylaRobot/modules/afk.py
@@ -151,7 +151,13 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK"
+
+__help__ = """
+*Away from group*
+ ❍ /afk <reason>*:* mark yourself as AFK(away from keyboard).
+"""
+
+__mod_name__ = "Afk"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
