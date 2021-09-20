5d233b1ad5aaf6c07f60be1964a098e8cca14485
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-09-20 19:35:20 +0800

    Update disable.py

--- LaylaRobot/modules/disable.py
+++ LaylaRobot/modules/disable.py
@@ -336,21 +336,19 @@ if is_module_loaded(FILENAME):
     dispatcher.add_handler(ENABLE_MODULE_HANDLER)
     dispatcher.add_handler(COMMANDS_HANDLER)
     dispatcher.add_handler(TOGGLE_HANDLER)
-    
-    
- __help__ = """
- ❍ /cmds*:* check the current status of disabled commands
 
- *Admins only:*
-    
- ❍ /enable <cmd name>*:* enable that command
- ❍ /disable <cmd name>*:* disable that command
- ❍ /enablemodule <module name>*:* enable all commands in that module
- ❍ /disablemodule <module name>*:* disable all commands in that module
- ❍ /listcmds*:* list all possible toggleable commands
- """
+    __help__ = """
+    ❍ /cmds*:* check the current status of disabled commands
 
- __mod_name__ = "Disable"
+    *Admins only:*
+    ❍ /enable <cmd name>*:* enable that command
+    ❍ /disable <cmd name>*:* disable that command
+    ❍ /enablemodule <module name>*:* enable all commands in that module
+    ❍ /disablemodule <module name>*:* disable all commands in that module
+    ❍ /listcmds*:* list all possible toggleable commands
+    """
+
+    __mod_name__ = "Disable"
 
 else:
     DisableAbleCommandHandler = CommandHandler
