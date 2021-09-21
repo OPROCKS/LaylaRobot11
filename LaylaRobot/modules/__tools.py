73754440e8ea8d8a2bf7d136e6509b68cab7bc8a
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-09-21 18:23:04 +0800

    Update __tools.py

--- LaylaRobot/modules/__tools.py
+++ LaylaRobot/modules/__tools.py
@@ -8,15 +8,35 @@ __help__ = """
 *Date-time-Weather*
  ❍ /time <country code>*:* Gives information about a timezone.
  ❍ /weather <city>*:* Get weather info in a particular place.
- ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
- ❍ /wttr moon*:* Get the current status of moon
  
 *Quotly*
-- /q : reply any message with this command to create a sticker.
+❍ /q : reply any message with this command to create a sticker.
 
 *Converts*
  ❍ /encrypt*:* Encrypts The Given Text
  ❍ /decrypt*:* Decrypts Previously Ecrypted Text
  ❍ /zip*:* reply to a telegram file to compress it in .zip format
  ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format
+ 
+*Markdown:*
+ ❍ /markdownhelp*:* quick summary of how markdown works in telegram - can only be called in private chats
+*Paste:*
+ ❍ /paste*:* Saves replied content to `nekobin.com` and replies with a url
+*React:*
+ ❍ /react*:* Reacts with a random reaction 
+*Urban Dictonary:*
+ ❍ /ud <word>*:* Type the word or expression you want to search use
+*Wikipedia:*
+ ❍ /wiki <query>*:* wikipedia your query
+*Wallpapers:*
+ ❍ /wall <query>*:* get a wallpaper from wall.alphacoders.com
+*live cricket score*
+ ❍ /cs*:* Latest live scores from cricinfo
+*Currency converter:* 
+ ❍ /cash*:* currency converter
+Example:
+ `/cash 1 USD INR`  
+      _OR_
+ `/cash 1 usd inr`
+Output: `1.0 USD = 75.505 INR`
 """
