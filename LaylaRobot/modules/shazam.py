89a99994fb183f2e8f0e0cf25afa33b57369ebca
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-09-21 15:04:35 +0800

    Update shazam.py

--- LaylaRobot/modules/shazam.py
+++ LaylaRobot/modules/shazam.py
@@ -2,7 +2,7 @@ import os
 from json import JSONDecodeError
 import requests
 from pyrogram import filters
-from LaylaRobot.pyrogramee.pluginhelper import admins_only, edit_or_reply, fetch_audio
+from LaylaRobot.pyrogramee.pluginshelper.py import admins_only, edit_or_reply, fetch_audio
 from LaylaRobot.pyrogramee.pyrogram import pbot
 
 
